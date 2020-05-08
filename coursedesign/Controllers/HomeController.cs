b374877da59bc8052c0e4b76e6894e0f580f2e34
wlyylw <931316452@qq.com>
2020-05-08 16:37:14 +0800

    add function add and alert

--- coursedesign/Controllers/HomeController.cs
+++ coursedesign/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
-﻿using coursedesign.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
@@ -11,6 +10,7 @@ namespace coursedesign.Controllers
 {
     public class HomeController : Controller
     {
+        DataEntity db = new DataEntity();
         public ActionResult Index()
         {
             return View();
@@ -20,16 +20,45 @@ namespace coursedesign.Controllers
         {
             return View();
         }
+        [HttpGet]
         public ActionResult AddData()
         {
             return View();
         }
-        public ActionResult DeleteData()
+        [HttpPost]
+        public ActionResult AddData(engineering model)
         {
-            return View();
+            try
+            {
+                engineering e = new engineering();
+                e.id = model.id;
+                e.name = model.name.Trim();
+                e.place = model.place.Trim();
+                e.salary = model.salary;
+                e.sex = model.sex;
+                e.telephone = model.telephone.Trim();
+                e.workage = model.workage.Trim();
+                e.address = model.address.Trim();
+                e.education = model.education;
+                e.birth = model.birth;
+                db.engineering.Add(e);
+                db.SaveChanges();
+                db.Configuration.ValidateOnSaveEnabled = true;
+                return RedirectToAction("AddData", "Home");
+            }
+            catch (Exception)
+            {
+                //指定对应跳转的视图Test下的Test.cshtml文件
+     /*           return RedirectToAction("AddData", "AddData");*/
+                return Content("添加失败" );
+            }
         }
-        public ActionResult AlertData()
+
+        public ActionResult EditData()
         {
+
+            List<engineering> list = (from c in db.engineering select c).ToList();
+            ViewData["DataList"] = list;
             return View();
         }
         public ActionResult CalculateData()
@@ -46,9 +75,7 @@ namespace coursedesign.Controllers
         }
         public ActionResult PrintData()
         {
-            DataEntity db = new DataEntity();
-            List<user> list = (from c in db.user select c).ToList();
-            ViewData["DataList"] = list;
+
             return View();
         }
         public ActionResult ReGetData()
@@ -58,6 +85,51 @@ namespace coursedesign.Controllers
         public ActionResult ExitSys()
         {
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult Modify()
+        {
+            int id = Convert.ToInt32(Request.QueryString["Cid"]);
+            engineering engineering = (from e in db.engineering where id == e.id select e).SingleOrDefault();
+            return View(engineering);
+        }
+        [HttpPost]
+        public ActionResult Modify(engineering model)
+        {
+            engineering e = db.engineering.Where(e1 => e1.id == model.id).ToList().FirstOrDefault();
+            e.name = model.name.Trim();
+            e.place = model.place.Trim();
+            e.salary = model.salary;
+            e.sex = model.sex;
+            e.telephone = model.telephone.Trim();
+            e.workage = model.workage.Trim();
+            e.address = model.address.Trim();
+            e.education = model.education;
+            e.birth = model.birth;
+            db.SaveChanges();
+            db.Configuration.ValidateOnSaveEnabled = true;
+            return RedirectToAction("EditData", "Home");
+        }
+        public ActionResult Remove()
+        {
+            try
+            {
+                //需要一个实体对象参数
+                //db.Customers.Remove(new Customer() {CustomerNo = id });
+                //1,创建要删除的对象
+                int id = Convert.ToInt32(Request.QueryString["Cid"]);
+                engineering engineering = (from e in db.engineering where e.id == id select e).SingleOrDefault();
+                db.engineering.Remove(engineering);
+                db.SaveChanges();
+                return RedirectToAction("EditData", "Home");
+            }
+            catch (Exception)
+            {
+                //指定对应跳转的视图Test下的Test.cshtml文件
+                return RedirectToAction("EditData", "EditData");
+                //return Content("删除失败" + ex.Message);
+            }
         }
     }
 }
