083e11f66a2927bb49fa69bf8a8631abdd5fb86d
wlyylw <931316452@qq.com>
2020-05-08 14:34:37 +0800

    add

---
+++ coursedesign/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using coursedesign.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.Mvc;
+
+namespace coursedesign.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult QueryData()
+        {
+            return View();
+        }
+        public ActionResult AddData()
+        {
+            return View();
+        }
+        public ActionResult DeleteData()
+        {
+            return View();
+        }
+        public ActionResult AlertData()
+        {
+            return View();
+        }
+        public ActionResult CalculateData()
+        {
+            return View();
+        }
+        public ActionResult SaveData()
+        {
+            return View();
+        }
+        public ActionResult ClearData()
+        {
+            return View();
+        }
+        public ActionResult PrintData()
+        {
+            DataEntity db = new DataEntity();
+            List<user> list = (from c in db.user select c).ToList();
+            ViewData["DataList"] = list;
+            return View();
+        }
+        public ActionResult ReGetData()
+        {
+            return View();
+        }
+        public ActionResult ExitSys()
+        {
+            return View();
+        }
+    }
+}
